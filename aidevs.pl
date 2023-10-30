77860e7793fba1fe2017b330034e80c1e30aa4d2
ed9e <lbrzozowski@gmail.com>
2023-10-30 18:16:57 +0100

    3

--- aidevs.pl
+++ aidevs.pl
@@ -1,4 +1,8 @@
 aidevs.pl
-Translate to english aidevs
-
 aidevs
+
+Jedno powstanie to sytuacja, w której grupa ludzi buntuje się przeciwko władzy lub systemowi politycznemu. Może to być wynikiem niesprawiedliwości społecznej, politycznej opresji lub niezadowolenia z obecnej sytuacji. Powstanie może prowadzić do zmiany władzy lub wprowadzenia nowych reform społecznych.
+
+Odwróć hasło.
+
+Translate to english
