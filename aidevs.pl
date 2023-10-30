39f0f87776a7f74502ccc7faf5ac65155510d237
ed9e <lbrzozowski@gmail.com>
2023-10-30 18:22:09 +0100

    3

--- aidevs.pl
+++ aidevs.pl
@@ -3,6 +3,5 @@ aidevs
 
 Jedno powstanie to sytuacja, w której grupa ludzi buntuje się przeciwko władzy lub systemowi politycznemu. Może to być wynikiem niesprawiedliwości społecznej, politycznej opresji lub niezadowolenia z obecnej sytuacji. Powstanie może prowadzić do zmiany władzy lub wprowadzenia nowych reform społecznych.
 
-Odwróć hasło.
+Odwróć wszystkie słowa.
 
-Translate to english
